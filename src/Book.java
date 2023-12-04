71958080795d0a0c7b80178c9b3614a037dafc37
Zelioforest <veshky@mail.ru>
2023-12-04 16:06:30 +0300

    attempt first

---
+++ src/Book.java
@@ -0,0 +1,27 @@
+public class Book {
+    private String title;
+    private Author author;
+    private int publishYear;
+
+    public Book(String title, Author author, int publishYear){
+        this.title = title;
+        this.author = author;
+        this.publishYear = publishYear;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public Author getAuthor(){
+        return this.author;
+    }
+
+    public int getPublishYear() {
+        return this.publishYear;
+    }
+
+    public void setPublishYear(int publishYear) {
+        this.publishYear = publishYear;
+    }
+}
